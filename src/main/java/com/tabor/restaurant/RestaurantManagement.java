71c7126689981d25ae380e094af54151a3006a45
Marek Tabor <Marek_Tabor@epam.com>
2018-04-16 17:02:39 +0200

    set receiver in kitchen. resgister order in waiter. need to figure out how to not close app and implement order system

--- src/main/java/com/tabor/restaurant/RestaurantManagement.java
+++ src/main/java/com/tabor/restaurant/RestaurantManagement.java
@@ -1,10 +1,13 @@
 package com.tabor.restaurant;
 
 public class RestaurantManagement implements RestaurantManagementInterface {
+    private KitchenInterface kitchen;
+    private ReceiverInterface receiver;
+    private OrderInterface order = new OrderInterfaceImpl();
 
     @Override
     public void addWaiter(WaiterInterface waiter) {
-
+        waiter.registerOrder(order);
     }
 
     @Override
@@ -14,6 +17,27 @@ public class RestaurantManagement implements RestaurantManagementInterface {
 
     @Override
     public void setKitchen(KitchenInterface kitchen) {
+        this.receiver = new ReceiverInterfaceImpl();
+        this.kitchen = kitchen;
+        this.kitchen.registerReceiver(receiver);
+    }
 
+    class ReceiverInterfaceImpl implements ReceiverInterface {
+        @Override
+        public void mealReady(int orderID) {
+
+        }
+    }
+
+    class OrderInterfaceImpl implements OrderInterface {
+        @Override
+        public void newOrder(int orderID, int tableID) {
+
+        }
+
+        @Override
+        public void orderComplete(int orderID, int tableID) {
+
+        }
     }
 }
